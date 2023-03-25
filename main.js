c60a67e2cd100d41f2d8d1e563cf2f141d078685
RezaHungkul <Rezahungkul12345@gmail.com>
2023-03-26 06:12:11 +0700

    add about

--- main.js
+++ main.js
@@ -4,10 +4,7 @@ function toggleDetail(e) {
         $(target).html("More Info").removeClass("active")
     } else {
         $(target).html("Less Info").addClass("active")
-
     }
-
-    
 
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
